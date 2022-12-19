a12023766303d0842d2f6f3e1e9de4d119b0f268
vojk <tmej.vojta@gmail.com>
2022-12-19 19:40:22 +0100

    Provedení úprav a zprovoznění získání dat z HTML dokumentu pomocí HTTP požadavku POST
    v app.py přidaný kod na získání požadavku POST
    v main.html jednoduchý formulář + lokální JS pro ukázání hodnoty ze slideru

--- app/app.py
+++ app/app.py
@@ -1,11 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return render_template('main.html', title='jahoda')
+def hello_world():  # main code base
+    posts = [{'title': 'Test', 'test': ''}]
+    return render_template('main.html', posts=posts)
+
+
+@app.route("/test", methods=["POST"])  # Určení cesty /test & nastavení metody POST
+def getRatingToText():  # Získání dat z formuláře v HTML dokumentu main.html
+    if request.method == "POST":  # Kontroluje zda-li je metoda POST
+        ratingSlider = request.form["_ratingSlider"]  # uloží data z _ratingSlider
+        nameTextInput = request.form["_nameOfElement"]  # uloží data z _nameOfElement
+        # Používá se "name" z form-input
+        return 'rating: ' + ratingSlider + ' name: ' + nameTextInput  # navrátí hodnoty z ratingSlider a nameTextInput
 
 
 if __name__ == '__main__':
