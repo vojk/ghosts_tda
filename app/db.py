40acfe03e617515d26731658cfed7ec5269e7166
vojk <tmej.vojta@gmail.com>
2022-12-22 14:21:15 +0100

    Opravený bug, který se objevoval, když byla aplikace zaplá v DOCKERu.

--- app/db.py
+++ app/db.py
@@ -4,11 +4,11 @@ import os
 
 user = "default"  # základní uživatel
 
-databaseExists = os.path.exists('./dbs/database.db')
+databaseExists = os.path.exists('app/dbs/database.db')
 
 if not databaseExists:  # ověřuje existenci databáze - zda-li existuje soubor database.db tak se nebude tvořit znova celá tabulka
-    connection = sqlite3.connect('./dbs/database.db')  # připojí se a případně vytvoří databázi ./dbs/database.db
-    with open('./dbs/schema.sql') as f:  # Vytvoří tabulku podle ./dbs/schema.sql
+    connection = sqlite3.connect('app/dbs/database.db')  # připojí se a případně vytvoří databázi ./dbs/database.db
+    with open('app/dbs/schema.sql') as f:  # Vytvoří tabulku podle ./dbs/schema.sql
         # connection.executescript(f.read())
         connection.executescript(
             'CREATE TABLE ' + "u_" + user +
@@ -20,13 +20,13 @@ if not databaseExists:  # ověřuje existenci databáze - zda-li existuje soubor
 
 
 def get_db_connection():
-    conn = sqlite3.connect('./dbs/database.db')
+    conn = sqlite3.connect('app/dbs/database.db')
     conn.row_factory = sqlite3.Row
     return conn
 
 
 def read_data_from_db(table):  # čte data z databáze
-    conn = sqlite3.connect('./dbs/database.db')
+    conn = sqlite3.connect('app/dbs/database.db')
     conn.row_factory = sqlite3.Row
     posts = conn.execute('SELECT * FROM ' + table).fetchall()
     conn.close()
