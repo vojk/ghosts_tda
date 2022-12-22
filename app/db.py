44958303dc12e82a3e64eaa1ad9ad1e4e11b68e3
vojk <tmej.vojta@gmail.com>
2022-12-22 19:03:20 +0100

    Bugfix bugfixu
    Opravený bug, který se objevoval, když byla aplikace zaplá v DOCKERu.

--- app/db.py
+++ app/db.py
@@ -4,15 +4,23 @@ import os
 
 user = "default"  # základní uživatel
 
-databaseExists = os.path.exists('app/dbs/database.db')
+databasePath = ''
+
+if os.name == 'posix':
+    databasePath = 'app/dbs/database.db'
+elif os.name == 'nt':
+    databasePath = './dbs/database.db'
+
+databaseExists = os.path.exists(databasePath)
+
 
 if not databaseExists:  # ověřuje existenci databáze - zda-li existuje soubor database.db tak se nebude tvořit znova celá tabulka
-    connection = sqlite3.connect('app/dbs/database.db')  # připojí se a případně vytvoří databázi ./dbs/database.db
-    with open('app/dbs/schema.sql') as f:  # Vytvoří tabulku podle ./dbs/schema.sql
+    connection = sqlite3.connect(databasePath)  # připojí se a případně vytvoří databázi ./dbs/database.db
+    with open('schema.sql') as f:  # Vytvoří tabulku podle ./dbs/schema.sql
         # connection.executescript(f.read())
         connection.executescript(
             'CREATE TABLE ' + "u_" + user +
-                ' (id INTEGER PRIMARY KEY AUTOINCREMENT, dates DATE NOT NULL, timeInMinutes INT NOT NULL, programmingLang TEXT NOT NULL, rating INT NOT NULL, description TEXT NOT NULL)')
+            ' (id INTEGER PRIMARY KEY AUTOINCREMENT, dates DATE NOT NULL, timeInMinutes INT NOT NULL, programmingLang TEXT NOT NULL, rating INT NOT NULL, description TEXT NOT NULL)')
 
     cur = connection.cursor()  # Vytvoří kursor
     connection.commit()  # vloží data do databáze
@@ -20,13 +28,13 @@ if not databaseExists:  # ověřuje existenci databáze - zda-li existuje soubor
 
 
 def get_db_connection():
-    conn = sqlite3.connect('app/dbs/database.db')
+    conn = sqlite3.connect(databasePath)
     conn.row_factory = sqlite3.Row
     return conn
 
 
 def read_data_from_db(table):  # čte data z databáze
-    conn = sqlite3.connect('app/dbs/database.db')
+    conn = sqlite3.connect(databasePath)
     conn.row_factory = sqlite3.Row
     posts = conn.execute('SELECT * FROM ' + table).fetchall()
     conn.close()
