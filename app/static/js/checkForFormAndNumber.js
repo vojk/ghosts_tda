a7c602c429c6c51b3e2055dbfac5bce719bdee6c
vojk <tmej.vojta@gmail.com>
2022-12-25 19:40:21 +0100

    Kontrola Null hodnot v createWind.html, při tvoření záznamu Je to i ready na edit záznamu (backend řešení)
    Přidaná knihovna jquery-3.6.3.min.js pro js

---
+++ app/static/js/checkForFormAndNumber.js
@@ -0,0 +1,52 @@
+let date = document.forms["addRecord"]["formDate"];
+let time = document.forms["addRecord"]["formMinutes"];
+let desc = document.forms["addRecord"]["formDesc"];
+
+function validateForm() {
+    if (date.value == "" && !(date.classList.contains('invalid'))) {
+        date.classList.add('invalid')
+    } else if (!(date.value == "")) {
+        date.classList.remove('invalid')
+    }
+
+    if (time.value == "" && !(time.classList.contains('invalid'))) {
+        time.classList.add('invalid')
+    } else if (!(time.value == "")) {
+        time.classList.remove('invalid')
+    }
+
+    if (desc.value == "" && !(desc.classList.contains('invalid'))) {
+        desc.classList.add('invalid')
+    } else if (!(desc.value == "")) {
+        desc.classList.remove('invalid')
+    }
+
+    if (date.value == "" || time.value == "" || desc.value == "") {
+        return false;
+    }
+}
+
+date.addEventListener('input', function (evt) {
+    if (this.value !== "") {
+        this.classList.remove('invalid')
+    }
+});
+
+time.addEventListener('input', function (evt) {
+    if (this.value !== "") {
+        this.classList.remove('invalid')
+    }
+});
+
+desc.addEventListener('input', function (evt) {
+    if (this.value !== "") {
+        this.classList.remove('invalid')
+    }
+});
+
+$('#_formMinutes').on('keypress', function (e) {
+    return e.metaKey || // cmd/ctrl
+        e.which <= 0 || // arrow keys
+        e.which == 8 || // delete key
+        /[0-9]/.test(String.fromCharCode(e.which)); // numbers
+})
