d65483a83bbcf90bd99b698be66652dfe092cfc8
Vojk <tmej.vojta@gmail.com>
2023-01-04 23:22:02 +0100

    Vyřešené sortování pomocí AJAX requstu, tím pádem se stránka nerefreshuje.
    V modaltest.html => ukázka jak funguje sortování hlavní část v body + scripty v head
    table_records.html => tabulka, která se bude updatovat
    
    app.py (názvy definicí se budou měnit)
    def sort slouží pro update tabulky
    def test slouží pro test samotné tabulky (odkaz na stránku)

--- app/app.py
+++ app/app.py
@@ -31,97 +31,106 @@ def app_wind():
                                                  filter_timeinminutes))
 
 
-# BackgroundTask
-@app.route('/background_process_test')
-def background_process_test():
-    print("Hello")
-    return "nothing"
+@app.route('/test')
+def test():
+    sort_field = request.args.get('sort_field')
+    return render_template('modaltest.html', texts=sorting.pre_sort(sort_field, None, None, None,
+                                                                    None))
 
 
-@app.route("/blank")
-def blank_site():
-    return render_template('update.html', text=db.read_data_from_db())
+@app.route('/sort')
+def sort():
+    sort_field = request.args.get('sort_field')
 
+    return render_template('table_records.html', texts=sorting.pre_sort(sort_field, None, None, None,
+                                                                        None))
 
-@app.route("/add/", methods=('GET', 'POST'))
-@app.route('/app/add/', methods=('GET', 'POST'))
-def create_record():
-    if request.method == 'POST':
-        date = request.form['formDate']
-        minutes = request.form['formMinutes']
-        rating = request.form['formRating']
-        progLang = request.form['formProgLang_select']
-        desc = request.form['formDesc']
+    # BackgroundTask
+    @app.route('/background_process_test')
+    def background_process_test():
+        print("Hello")
+        return "nothing"
 
-        if not date:
-            print("No date found")
-        elif not minutes or minutes == 0:
-            print("No minutes found")
-        elif progLang == "None" or not progLang:
-            print("No progLang found")
-        elif not desc:
-            print("No desc found")
-        else:
-            conn = db.get_db_connection()
-            conn.execute(
-                'INSERT INTO records (dates, timeInMinutes, programmingLang, rating, description, programmer) '
-                'VALUES (?, ?, ?, ?, ?, ?)', (date, minutes, progLang, rating, desc, 'u_default'))
-            conn.commit()
-            conn.close()
-            return redirect(url_for('app_wind'))
+    @app.route("/blank")
+    def blank_site():
+        return render_template('update.html', text=db.read_data_from_db())
 
-    return render_template('createWind.html', defs=proglangs)
+    @app.route("/add/", methods=('GET', 'POST'))
+    @app.route('/app/add/', methods=('GET', 'POST'))
+    def create_record():
+        if request.method == 'POST':
+            date = request.form['formDate']
+            minutes = request.form['formMinutes']
+            rating = request.form['formRating']
+            progLang = request.form['formProgLang_select']
+            desc = request.form['formDesc']
 
+            if not date:
+                print("No date found")
+            elif not minutes or minutes == 0:
+                print("No minutes found")
+            elif progLang == "None" or not progLang:
+                print("No progLang found")
+            elif not desc:
+                print("No desc found")
+            else:
+                conn = db.get_db_connection()
+                conn.execute(
+                    'INSERT INTO records (dates, timeInMinutes, programmingLang, rating, description, programmer) '
+                    'VALUES (?, ?, ?, ?, ?, ?)', (date, minutes, progLang, rating, desc, 'u_default'))
+                conn.commit()
+                conn.close()
+                return redirect(url_for('app_wind'))
 
-@app.route('/<int:id>/edit/', methods=('GET', 'POST'))
-@app.route('/app/<int:id>/edit/', methods=('GET', 'POST'))
-def edit(id):
-    record = db.get_data_from_db_by_id(id)
-    if not record:
-        abort(404)
+        return render_template('createWind.html', defs=proglangs)
 
-    if request.method == 'POST':
-        date = request.form['formDate']
-        minutes = request.form['formMinutes']
-        rating = request.form['formRating']
-        progLang = request.form['formProgLang_select']
-        desc = request.form['formDesc']
+    @app.route('/<int:id>/edit/', methods=('GET', 'POST'))
+    @app.route('/app/<int:id>/edit/', methods=('GET', 'POST'))
+    def edit(id):
+        record = db.get_data_from_db_by_id(id)
+        if not record:
+            abort(404)
 
-        if not date:
-            print("No date found")
-        elif not minutes or minutes == 0:
-            print("No minutes found")
-        elif progLang == "None" or not progLang:
-            print("No progLang found")
-        elif not desc:
-            print("No desc found")
-        else:
-            conn = db.get_db_connection()
-            conn.execute(
-                'UPDATE records SET dates = ?, timeInMinutes = ?, programmingLang = ?, rating = ?, description = ? '
-                'WHERE id = ?',
-                (date, minutes, progLang, rating, desc, id))
-            conn.commit()
-            conn.close()
-            return redirect(url_for('app_wind'))
+        if request.method == 'POST':
+            date = request.form['formDate']
+            minutes = request.form['formMinutes']
+            rating = request.form['formRating']
+            progLang = request.form['formProgLang_select']
+            desc = request.form['formDesc']
 
-    return render_template('editWind.html', record=record, defs=proglangs)
+            if not date:
+                print("No date found")
+            elif not minutes or minutes == 0:
+                print("No minutes found")
+            elif progLang == "None" or not progLang:
+                print("No progLang found")
+            elif not desc:
+                print("No desc found")
+            else:
+                conn = db.get_db_connection()
+                conn.execute(
+                    'UPDATE records SET dates = ?, timeInMinutes = ?, programmingLang = ?, rating = ?, description = ? '
+                    'WHERE id = ?',
+                    (date, minutes, progLang, rating, desc, id))
+                conn.commit()
+                conn.close()
+                return redirect(url_for('app_wind'))
 
+        return render_template('editWind.html', record=record, defs=proglangs)
 
-@app.route('/<int:id>/delete/', methods=('GET', 'POST'))
-@app.route('/app/<int:id>/delete/', methods=('GET', 'POST'))
-def delete(id):
-    record = db.get_data_from_db_by_id(id)
-    if not record:
-        abort(404)
+    @app.route('/<int:id>/delete/', methods=('GET', 'POST'))
+    @app.route('/app/<int:id>/delete/', methods=('GET', 'POST'))
+    def delete(id):
+        record = db.get_data_from_db_by_id(id)
+        if not record:
+            abort(404)
 
-    conn = db.get_db_connection()
-    conn.execute('DELETE FROM records WHERE id = ?', (id,))
-    conn.commit()
-    conn.close()
-    return redirect(url_for('app_wind'))
+        conn = db.get_db_connection()
+        conn.execute('DELETE FROM records WHERE id = ?', (id,))
+        conn.commit()
+        conn.close()
+        return redirect(url_for('app_wind'))
 
-
-if __name__ == '__main__':
-    app.secret_key = secrets.token_hex(16)
-    app.run()
+    if __name__ == '__main__':
+        app.secret_key = secrets.token_hex(16)
+        app.run()
