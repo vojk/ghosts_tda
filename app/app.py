32c2db5707608b80705e98c411f8aaedda0806e7
vojk <tmej.vojta@gmail.com>
2023-03-31 14:15:32 +0200

    Oprava bugu, login manager smazanej

--- app/app.py
+++ app/app.py
@@ -9,20 +9,11 @@ from werkzeug.security import check_password_hash, generate_password_hash
 import db
 import secrets
 
-pre_proglangs = [{'progLangs': 'Java'}, {'progLangs': 'Python'}, {'progLangs': 'C'}, {'progLangs': 'Ruby'},
-                 {'progLangs': 'JavaScript'}, {'progLangs': 'TypeScript'}, {'progLangs': 'Kotlin'},
-                 {'progLangs': 'Swift'}, {'progLangs': 'PHP'}, {'progLangs': 'Go'}]
-list_of_elements = ["id", "date", "time_spent", "programming_language", "rating", "description"]
-
-proglangs = sorted(pre_proglangs, key=lambda x: x['progLangs'])
-
 secret_key = "61c42d54bdc57fdb8fa03af867511dff"
 
 app = Flask(__name__)
 app.config['JSON_SORT_KEYS'] = False
 app.secret_key = secret_key
-login_manager = LoginManager()
-login_manager.init_app(app)
 
 db  # inicializuje databázi
 
@@ -32,7 +23,6 @@ db  # inicializuje databázi
 #  něco to udělá ale nevím co
 @app.route('/')  # main app lokalizace
 @app.route('/app/', methods=["GET", "POST"])
-@login_required
 def app_wind():
     conn = db.get_db_connection()
     conn.close()
